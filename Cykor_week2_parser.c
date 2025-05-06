3d50fa6aafad516bd2060d92c6ad6e7e8abe5427
imshigaejung <imshigaejung@naver.com>
2025-05-06 12:33:24 +0900

    Update Cykor_week2_parser.c

--- Cykor_week2_parser.c
+++ Cykor_week2_parser.c
@@ -4,8 +4,44 @@
 #include <ctype.h>
 #include "Cykor_week2_parser.h"
 
+int scan_tokens(const char*chunk, TokenInfo *tokens, int* token_count){
+    int i = 0;
+    int count = 0;
+    int in_quote = 0;
+    char quote_char = '\0';
+
+    while (chunk[i] != '\0') {
+
+        if (isspace(chunk[i])) {
+            i++;
+            continue;
+        }
+
+        int start = i;
+
+        if(in_quote == 0 && (chunk[i] == '\''||chunk[i]=='"')){
+            quote_char = chunk[i];
+            in_quote = 1;
+            while(chunk[i]&&chunk[i] != quote_char){
+                i++;
+            }
+            if(chunk[i] == quote_char && in_quote == 1){
+                in_quote = 0;
+                tokens[count++] = (TokenInfo){PARAM, start, i - 1};
+            }
+        }
+  
+        while(chunk[i] && !isspace(chunk[i])){
+            i++;
+        }
+        tokens[count++] = (TokenInfo){PARAM, start, i - 1};
+
+    }
+    *token_count = count;
+    return 0;
+}
 //입력받은 문자열을 문자 단위로 분석 --> 
-int scan_tokens(const char *input, TokenInfo *tokens, int *token_count) {
+int scan_chunk(const char *input, ChunkInfo *chunk, int *chunk_count) {
     int i = 0;
     int count = 0;
 
@@ -20,34 +56,40 @@ int scan_tokens(const char *input, TokenInfo *tokens, int *token_count) {
         // |와 ||를 구분
         if (input[i] == '|') {
             if (input[i + 1] == '|') {
-                tokens[count++] = (TokenInfo){OR, i, i + 1};
+                chunk[count++] = (ChunkInfo){OR, i, i + 1};
                 i += 2;
             } else {
-                tokens[count++] = (TokenInfo){PIPE, i, i};
+                chunk[count++] = (ChunkInfo){PIPE, i, i};
                 i++;
             }
         }
         // &와 &&를 구분
         else if (input[i] == '&') {
             if (input[i + 1] == '&') {
-                tokens[count++] = (TokenInfo){AND, i, i + 1};
+                chunk[count++] = (ChunkInfo){AND, i, i + 1};
                 i += 2;
             } else {
-                tokens[count++] = (TokenInfo){BACK, i, i};
+                chunk[count++] = (ChunkInfo){BACK, i, i};
                 i++;
             }
+        }
+        // ;를 구분
+        else if (input[i] == ';') {
+            chunk[count++] = (ChunkInfo){SEPERATOR, i, i};
+            i++;
+            
         }
         //구분자가 아닌 문자열에 대하여 명령어로 인식
         else {
             int start = i;
-            while (input[i] && !isspace(input[i]) && input[i] != '|' && input[i] != '&') {
+            while (input[i] && input[i] != '|' && input[i] != '&' && input[i] != ';') {
                 i++;
             }
-            tokens[count++] = (TokenInfo){CMD_NONE, start, i - 1};
+            chunk[count++] = (ChunkInfo){CHUNK, start, i - 1};
         }
     }
 
-    *token_count = count;
+    *chunk_count = count;
     return 0;
 }
 
@@ -69,6 +111,27 @@ char **build_token_array(const char *input, TokenInfo *tokens, int token_count) 
     }
 
     result[token_count] = NULL;  // execvp 호환 가능하게
+    return result;
+}
+
+char **build_chunk_array(const char *input, ChunkInfo*chunk, int chunk_count) {
+    char **result = malloc(sizeof(char *) * (chunk_count + 1));  // 널문자 입력을 위해 1만큼 큰 바이트 할당
+    if (!result) return NULL;
+
+    for (int i = 0; i < chunk_count; i++) {
+        int len = chunk[i].end - chunk[i].start + 1; // 널 문자를 집어넣기 위해 길이를 문자열 크기보다 1만큼 크게 설정정
+        result[i] = malloc(len + 1);
+        if (!result[i]) {
+            // 해제 후 종료
+            for (int j = 0; j < i; j++) free(result[j]);
+            free(result);
+            return NULL;
+        }
+        strncpy(result[i], &input[chunk[i].start], len);
+        result[i][len] = '\0';  // 널 문자 삽입 후 종료
+    }
+
+    result[chunk_count] = NULL;  // execvp 호환 가능하게
     return result;
 }
 
