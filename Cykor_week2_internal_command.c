993eaa6db7f18dba36b2598c523cb5693daa8165
imshigaejung <imshigaejung@naver.com>
2025-05-06 12:30:02 +0900

    Update Cykor_week2_internal_command.c

--- Cykor_week2_internal_command.c
+++ Cykor_week2_internal_command.c
@@ -4,37 +4,43 @@
 #include <stdlib.h>
 
 
-void execute_cd(char **cmd_args) {
+int execute_cd(char **cmd_args) {
     if (cmd_args[1] == NULL) {
         // 인자 없으면 HOME 디렉토리로 이동
         char *home = getenv("HOME");
         if (home != NULL) {
             if (chdir(home) != 0) {
                 perror("cd");
+                return 0;
             }
+            return 1;
         } else {
             fprintf(stderr, "cd: HOME not set\n");
+            return 0;
         }
     } else {
         // 인자(args[1])는 동적 할당된 문자열
         if (chdir(cmd_args[1]) != 0) {
             perror("cd");
+            return 0;
         }
-        // ⚡ 여기서는 args[1]을 free하면 안 된다!
+        return 1;
     }
 }
 
-void execute_pwd(){
+int execute_pwd(){
     char*cwd = getcwd(NULL,0);
 
     if (cwd != NULL) {
         printf("%s\n", cwd);
         free(cwd);  // 동적 메모리 해제
+        return 1;
     } else {
         perror("pwd");
+        return 0;
     }
 }
 
-void execute_exit(){
+int execute_exit(){
     exit(0);
 }
