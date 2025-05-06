6f96237b460784d3d6714a7b5aee21da868df466
imshigaejung <imshigaejung@naver.com>
2025-05-06 12:31:38 +0900

    Update Cykor_week2_main.c

--- Cykor_week2_main.c
+++ Cykor_week2_main.c
@@ -10,8 +10,9 @@
 #include <pwd.h>
 
 // 현재 토큰의 개수는 정적으로 선언되어있지만, 후에 동적으로 바꿔야 함.
-#define MAX_TOKENS 120
-const char* token_type_to_str(TokenType type);
+
+#define MAX_CHUNK 120
+const char* token_type_to_str(ChunkType type);
 
 typedef struct user_info
 {
@@ -74,10 +75,13 @@ void print_prompt(info user_informaition) {
 
 int main(){
     char *input;
-    char **parsed_array;
+    char **chunk_array;
     static info user_information; 
-    TokenInfo tokens[MAX_TOKENS];
-    int token_count = 0;
+    ChunkInfo chunk[MAX_CHUNK];
+    
+
+    
+    int chunk_count = 0;
 
 
     Login(&user_information);
@@ -87,34 +91,33 @@ int main(){
         input = dynamic_input();
     
         //문자열 스캔 --> 구분자, 공백, 명령어 구분
-        scan_tokens(input, tokens, &token_count);
+        scan_chunk(input, chunk, &chunk_count);
 
         //스캔한 문자열을 토큰화 시켜서 parsed_array에 저장
-        parsed_array = build_token_array(input, tokens, token_count);
+        chunk_array = build_chunk_array(input, chunk, chunk_count);
 
         //이제 input에 할당 되었던 메모리 다시 해제
         free(input);
         
-        //명령어를 내장 명령어, 외부 명령어, 지시 사항으로 분리
-        scan_command(parsed_array, tokens, token_count);
+        /*//명령어를 내장 명령어, 외부 명령어, 지시 사항으로 분리
+        scan_command(chunk_array, chunk, chunk_count);*/
 
         //토큰들을 명령어 처리 함수로 넘김
-        execute_command(parsed_array, tokens, token_count);
+        execute_command(chunk_array, chunk, chunk_count,0);
 
         //입력된 문자열이 잘 파싱되었는지 확인 차 출력
-        /*printf("Parsed Array:\n");
-        for (int i = 0; parsed_array[i] != NULL; i++) {
-        printf("  [%d]: %s - %s\n", i, parsed_array[i], token_type_to_str(tokens[i].type));
+        printf("Parsed Array:\n");
+        for (int i = 0; chunk_array[i] != NULL; i++) {
+        printf("  [%d]: %s - %s\n", i, chunk_array[i], token_type_to_str(chunk[i].type));
 
-        }*/
+        }
     }
 
 }
-const char* token_type_to_str(TokenType type) {
+const char* token_type_to_str(ChunkType type) {
     switch(type) {
-        case CMD_NONE: return "CMD_NONE";
-        case CMD_INTERNAL: return "CMD_INTERNAL";
-        case CMD_EXTERNAL: return "CMD_EXTERNAL";
+        case SEPERATOR: return "SEPERATOR";
+        case CHUNK: return "CHUNK";
         case AND: return "AND";
         case OR: return "OR";
         case BACK: return "BACK";
