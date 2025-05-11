807fedcf593cac138b1200d72ca501ddd2375428
imshigaejung <imshigaejung@naver.com>
2025-05-12 00:01:38 +0900

    Update Cykor_week2_external_command.h

--- Cykor_week2_external_command.h
+++ Cykor_week2_external_command.h
@@ -1,6 +1,6 @@
 #ifndef EXTERNAL_COMMAND_H
 #define EXTERNAL_COMMAND_H
 
-int execute_external_command(char **argv);
+int execute_external_command(char **argv, int is_background, int *job_id);
 
 #endif
