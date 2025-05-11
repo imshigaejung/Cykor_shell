2491bb9d023804a099517299c1bb4dad8393e761
imshigaejung <imshigaejung@naver.com>
2025-05-12 00:00:45 +0900

    Update Cykor_week2_executor.h

--- Cykor_week2_executor.h
+++ Cykor_week2_executor.h
@@ -5,10 +5,11 @@
 
 
 int is_command(const char*command, TokenInfo*tokens);
-int execute_command(char **command_chunk, ChunkInfo *chunk, int chunk_count, int chunk_num);
+int command_branch(char **command_chunk, ChunkInfo *chunk, int chunk_count, int chunk_num);
+int execute_command(char ** token_array, TokenInfo *tokens, int is_background);
 int execute_builtin_command(char**cmd_args);
 int should_execute_next(int status, ChunkType operator);
-
+int execute_pipe(char** cmd_args, ChunkInfo *pipe_chunk, int pipe_count, int is_background);
 
 
 #endif
